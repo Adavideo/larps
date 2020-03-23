f263b79f851b6234ac1f08613bc562c8bd5c9f93
adavideo <adavideo@gmail.com>
2020-03-24 00:18:45 +0100

    Access the profile asociated to the current user

--- larps/views.py
+++ larps/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.urls import reverse
 from django.views import generic
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .forms import PlayerForm
 from .models import Character, Player, CharacterAssigment, Bookings, Group, DietaryRestriction
@@ -33,20 +34,23 @@ class BookingsListView(generic.ListView):
     def get_queryset(self):
         return Bookings.objects.all()
 
-def get_user_profile():
-    player = Player.objects.all()[0]
+def get_player_profile(user):
+    profiles = Player.objects.filter(user=user)
+    if len(profiles) == 0:
+        player = Player(user=user)
+    else:
+        player = profiles[0]
     return player
 
 @login_required
 def player_profile(request):
-    print("Autenticado")
+    player = get_player_profile(request.user)
     if request.method == 'POST':
         form = PlayerForm(request.POST)
         if form.is_valid():
-            player = get_user_profile()
             player.save_profile(form.cleaned_data)
             return HttpResponseRedirect('/larps/players')
     else:
-        player_data = get_user_profile().get_data()
+        player_data = player.get_data()
         form = PlayerForm(player_data)
     return render(request, 'larps/player_profile.html', {'form': form})
