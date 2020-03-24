150605820098750ef98c31bf9b8495805916318e
adavideo <adavideo@gmail.com>
2020-03-24 20:17:22 +0100

    Adding navigation menu to the system

--- larps/urls.py
+++ larps/urls.py
@@ -5,13 +5,13 @@ from . import views
 app_name = 'larps'
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('player/<int:pk>/', views.PlayerView.as_view(), name='player'),
-    path('players', views.PlayersListView.as_view(), name='playerslist'),
-    path('character/<int:pk>/', views.CharacterView.as_view(), name='character'),
-    path('characters', views.CharactersListView.as_view(), name='characterslist'),
-    path('bookings', views.BookingsListView.as_view(), name='bookingslist'),
-    path('bookings/<int:pk>/', views.BookingsView.as_view(), name='bookings'),
-    path('bookings/larp_<int:larp_id>/', views.manage_bookings, name='manage_bookings'),
     path('player_profile/', views.player_profile, name='player_profile'),
+    path('player/<int:pk>/', views.PlayerView.as_view(), name='player'),
+    path('players', views.PlayersListView.as_view(), name='players_list'),
+    path('character/<int:pk>/', views.CharacterView.as_view(), name='character'),
+    path('characters', views.CharactersListView.as_view(), name='characters_list'),
+    path('bookings', views.BookingsListView.as_view(), name='bookings_list'),
+    path('bookings/<int:pk>/', views.BookingsView.as_view(), name='bookings'),
+    path('bookings/larp_<int:larp_id>/', views.manage_bookings, name='manage_bookings'),    
     path('logout/', views.logout_view, name='logout'),
 ]
