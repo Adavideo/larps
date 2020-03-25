2ecbc952f8696e9abaa271e290d9bda7edf2aad0
adavideo <adavideo@gmail.com>
2020-03-25 10:19:03 +0100

    Force login for most web pages

--- larps/urls.py
+++ larps/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 app_name = 'larps'
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('player_profile/', views.player_profile, name='player_profile'),
-    path('players', views.PlayersListView.as_view(), name='players_list'),
-    path('character/<int:pk>/', views.CharacterView.as_view(), name='character'),
-    path('characters', views.CharactersListView.as_view(), name='characters_list'),
-    path('bookings', views.BookingsListView.as_view(), name='bookings_list'),
-    path('bookings/<int:pk>/', views.BookingsView.as_view(), name='bookings'),
-    path('bookings/larp_<int:larp_id>/', views.manage_bookings, name='manage_bookings'),
+    path('player_profile/', login_required(views.player_profile), name='player_profile'),
+    path('players', login_required(views.PlayersListView.as_view()), name='players_list'),
+    path('character/<int:pk>/', login_required(views.CharacterView.as_view()), name='character'),
+    path('characters', login_required(views.CharactersListView.as_view()), name='characters_list'),
+    path('bookings', login_required(views.BookingsListView.as_view()), name='bookings_list'),
+    path('bookings/<int:pk>/', login_required(views.BookingsView.as_view()), name='bookings'),
+    path('bookings/larp_<int:larp_id>/', login_required(views.manage_bookings), name='manage_bookings'),
     path('logout/', views.logout_view, name='logout'),
 ]
