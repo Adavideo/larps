7e5aa804f27870b6cb6612003078264dcb177c8a
adavideo <adavideo@gmail.com>
2020-03-27 12:35:49 +0100

    Separated tests for views with no login

--- larps/test_views.py
+++ larps/test_views.py
@@ -1,18 +1,10 @@
 from django.test import TestCase
 from django.urls import reverse
-from .util_test_views import *
+from .util_test_views import test_correct_page, test_login
 
 
 class ViewsTests(TestCase):
 
-    # HOME PAGE
-
-    def test_home_page(self):
-        """
-            home_page() Test the home page is working
-        """
-        url = reverse('larps:home')
-        test_correct_page(self, url)
 
     def test_login_page(self):
         test_login(self)
@@ -20,79 +12,55 @@ class ViewsTests(TestCase):
 
     # PLAYER PROFILE
 
-    def test_player_profile_page_no_login(self):
-        """
-            player_profile_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_player_profile_page(self):
         url = reverse('larps:player_profile')
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # PLAYERS LIST
 
-    def test_players_list_page_no_login(self):
-        """
-            players_list_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_players_list_page(self):
         url = reverse('larps:players_list')
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # CHARACTER PAGE
 
-    def test_character_page_no_login(self):
-        """
-            character_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_character_page(self):
         url = "/larps/character/1/"
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # CHARACTERS LIST
 
-    def test_characters_list_page_no_login(self):
-        """
-            characters_list_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_characters_list_page(self):
         url = reverse('larps:characters_list')
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # BOOKINGS PAGE
 
-    def test_bookings_page_no_login(self):
-        """
-            bookings_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_bookings_page(self):
         url = "/larps/bookings/1/"
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # MANAGE BOOKINGS PAGE
 
-    def test_manage_bookings_page_no_login(self):
-        """
-            bookings_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_manage_bookings_page(self):
         url = "/larps/bookings/larp_1/run_1/"
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # BOOKINGS LIST
 
-    def test_bookings_list_page_no_login(self):
-        """
-            bookings_list_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_bookings_list_page(self):
         url = reverse('larps:bookings_list')
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
 
 
     # PAGE FOR CSV FILE UPLOAD
 
-    def test_csv_upload_page_no_login(self):
-        """
-            csv_upload_page_no_login() checks that it redirects to login when trying to access this page anonimously.
-        """
+    def test_csv_upload_page(self):
         url = reverse('larps:file_upload')
-        test_page_no_login(self, url)
+        test_correct_page(self, url)
---
+++ larps/test_views_no_login.py
@@ -0,0 +1,95 @@
+from django.test import TestCase
+from django.urls import reverse
+from .util_test_views import test_correct_page, test_page_no_login
+
+
+class ViewsTestsNoLogin(TestCase):
+
+    # HOME PAGE
+
+    def test_home_page(self):
+        """
+            home_page() Test the home page is working
+        """
+        url = reverse('larps:home')
+        test_correct_page(self, url)
+
+
+    # PLAYER PROFILE
+
+    def test_player_profile_page_no_login(self):
+        """
+            player_profile_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = reverse('larps:player_profile')
+        test_page_no_login(self, url)
+
+
+    # PLAYERS LIST
+
+    def test_players_list_page_no_login(self):
+        """
+            players_list_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = reverse('larps:players_list')
+        test_page_no_login(self, url)
+
+
+    # CHARACTER PAGE
+
+    def test_character_page_no_login(self):
+        """
+            character_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = "/larps/character/1/"
+        test_page_no_login(self, url)
+
+
+    # CHARACTERS LIST
+
+    def test_characters_list_page_no_login(self):
+        """
+            characters_list_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = reverse('larps:characters_list')
+        test_page_no_login(self, url)
+
+
+    # BOOKINGS PAGE
+
+    def test_bookings_page_no_login(self):
+        """
+            bookings_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = "/larps/bookings/1/"
+        test_page_no_login(self, url)
+
+
+    # MANAGE BOOKINGS PAGE
+
+    def test_manage_bookings_page_no_login(self):
+        """
+            bookings_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = "/larps/bookings/larp_1/run_1/"
+        test_page_no_login(self, url)
+
+
+    # BOOKINGS LIST
+
+    def test_bookings_list_page_no_login(self):
+        """
+            bookings_list_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = reverse('larps:bookings_list')
+        test_page_no_login(self, url)
+
+
+    # PAGE FOR CSV FILE UPLOAD
+
+    def test_csv_upload_page_no_login(self):
+        """
+            csv_upload_page_no_login() checks that it redirects to login when trying to access this page anonimously.
+        """
+        url = reverse('larps:file_upload')
+        test_page_no_login(self, url)
