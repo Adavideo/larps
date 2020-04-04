7f79e9b6b932f06e9f13e74b1981ede42f6083d0
adavideo <adavideo@gmail.com>
2020-04-05 00:51:12 +0200

    Added tests for menu amounts in food page

--- larps/tests/test_food.py
+++ larps/tests/test_food.py
@@ -1,21 +1,26 @@
 from django.test import TestCase
-from larps.models import Group, Larp, DietaryRestriction
+from larps.models import Group, Larp, DietaryRestriction, Player
 from .util_test import create_diets, create_characters_assigments
 from larps.food import *
 
 
 example_players_with_diets1 = [
-    { "username": "Ana_Garcia", "first_name": "Ana", "last_name": "Garcia", "gender":"female", "chest":90, "waist":75, "diet":"" },
+    { "username": "Ana_Garcia", "first_name": "Ana", "last_name": "Garcia", "gender":"female", "chest":90, "waist":75, "diet":"none" },
     { "username": "Pepa_Perez", "first_name": "Pepa", "last_name": "Perez", "gender":"female", "chest":95, "waist":78, "diet":"Vegan" },
-    { "username": "Manolo_Garcia", "first_name": "Manolo", "last_name": "Garcia", "gender":"male", "chest":100, "waist":90, "diet":"" },
+    { "username": "Manolo_Garcia", "first_name": "Manolo", "last_name": "Garcia", "gender":"male", "chest":100, "waist":90, "diet":"none" },
     { "username": "Paco_Garcia", "first_name": "Paco", "last_name": "Garcia", "gender":"male", "chest":102, "waist":86, "diet":"Vegan" },
 ]
 
 example_players_with_diets2 = [
-    { "username": "Maria_Gonzalez", "first_name": "Maria", "last_name": "Gonzalez", "gender":"", "chest":0, "waist":0, "diet":"Vegetarian" },
-    { "username": "Andrea_Hernandez", "first_name": "Andrea", "last_name": "Hernandez", "gender":"", "chest":0, "waist":0, "diet":"" },
-    { "username": "Juan_Perez", "first_name": "Juan", "last_name": "Perez", "gender":"male", "chest":100, "waist":90, "diet":"" },
+    { "username": "Maria_Gonzalez", "first_name": "Maria", "last_name": "Gonzalez", "gender":"female", "chest":0, "waist":0, "diet":"Vegetarian" },
+    { "username": "Andrea_Hernandez", "first_name": "Andrea", "last_name": "Hernandez", "gender":"female", "chest":0, "waist":0, "diet":"none" },
+    { "username": "Juan_Perez", "first_name": "Juan", "last_name": "Perez", "gender":"male", "chest":100, "waist":90, "diet":"none" },
     { "username": "Carlos_Hernandez", "first_name": "Carlos", "last_name": "Hernandez", "gender":"male", "chest":102, "waist":86, "diet":"Vegan" },
+]
+
+example_diets_counts = [
+    { "none" : 2, "Vegetarian": 1, "Vegan": 1 },
+    { "none" : 2, "Vegetarian": 0, "Vegan": 2 },
 ]
 
 example_players_incomplete = [
@@ -23,20 +28,18 @@ example_players_incomplete = [
     { "username": "Andrea_Hernandez", "first_name": "Andrea", "last_name": "Hernandez", "gender":"", "chest":0, "waist":0, "diet":"" },
 ]
 
-diets = [ "", "Vegetarian", "Vegan" ]
+diets = [ "Vegetarian", "Vegan", "none" ]
 
-example_characters = [ "Marie Curie", "Ada Lovelace", "Mary Jane Watson", "May Parker", "Peter Parker", "Leopold Fitz" ]
-
+example_characters1 = [ "Marie Curie", "Ada Lovelace", "Mary Jane Watson", "May Parker" ]
+example_characters2 = [ "Peter Parker", "Leopold Fitz", "Tony Stark", "Gemma Simmons" ]
 
 def create_group_with_diet_info(larp, players_info, characters_names, group_name=""):
     group = Group(larp=larp, name=group_name)
     group.save()
     players_info1 = [ players_info[0], players_info[1] ]
     players_info2 = [ players_info[2], players_info[3] ]
-    characters_names1 = [ characters_names[0], characters_names[1] ]
-    characters_names2 = [ characters_names[2], characters_names[3] ]
-    create_characters_assigments(group, players_info1, characters_names1, run=1)
-    create_characters_assigments(group, players_info2, characters_names1, run=2)
+    create_characters_assigments(group, players_info1, characters_names, run=1)
+    create_characters_assigments(group, players_info2, characters_names, run=2)
 
 def create_larp_with_diet_info(players_info, characters_names, group_name="", larp_name=""):
     create_diets(diets)
@@ -53,12 +56,12 @@ def test_character_diets(test, original_list, returned_info):
     test.assertEqual(returned_info["diet"], original_info["diet"])
 
 def create_all_1_group():
-    larp = create_larp_with_diet_info(example_players_with_diets1, example_characters, group_name="group1", larp_name="")
+    larp = create_larp_with_diet_info(example_players_with_diets1, example_characters1, group_name="group1")
     return larp
 
 def create_all_2_groups():
     larp = create_all_1_group()
-    create_group_with_diet_info(larp, example_players_with_diets2, example_characters, group_name="group2")
+    create_group_with_diet_info(larp, example_players_with_diets2, example_characters2, group_name="group2")
     return larp
 
 def create_all_without_profiles():
@@ -66,7 +69,7 @@ def create_all_without_profiles():
     larp.save()
     group = Group(larp=larp, name="")
     group.save()
-    create_characters_assigments(group, example_players_incomplete, example_characters, run=1)
+    create_characters_assigments(group, example_players_incomplete, example_characters1, run=1)
     return larp
 
 
@@ -115,6 +118,60 @@ class FoodTests(TestCase):
         self.assertEqual(len(players_diets_all_runs), 1)
         for player_diet in players_diets_all_runs[0]:
             self.assertEqual(player_diet["diet"], "Not provided yet")
+
+    def test_get_diets_amounts_no_diets(self):
+        # Initialize
+        larp = create_all_without_profiles()
+        assigments = larp.get_character_assigments()
+        number_of_runs = 1
+        # Obtain results
+        assigments = larp.get_character_assigments()
+        number_of_runs = get_runs_number(assigments)
+        players_diets = get_players_diets(assigments, number_of_runs)
+        diets_types = DietaryRestriction.objects.all()
+        all_runs_diets = get_diets_amounts(players_diets, number_of_runs, diets_types)
+        # Validate
+        self.assertIs(len(all_runs_diets), 1)
+        for run_diets in all_runs_diets:
+            self.assertEqual(run_diets, [])
+
+    def test_get_diets_amounts_empty(self):
+        # Initialize
+        create_diets(diets)
+        larp = create_all_without_profiles()
+        assigments = larp.get_character_assigments()
+        number_of_runs = 1
+        # Obtain results
+        assigments = larp.get_character_assigments()
+        number_of_runs = get_runs_number(assigments)
+        players_diets = get_players_diets(assigments, number_of_runs)
+        diets_types = DietaryRestriction.objects.all()
+        all_runs_diets = get_diets_amounts(players_diets, number_of_runs, diets_types)
+        # Validate
+        self.assertIs(len(all_runs_diets), 1)
+        for run_diets in all_runs_diets:
+            for diet in run_diets:
+                diet_name = diet["diet_name"]
+                self.assertIs(diet["amount"], 0)
+
+    def test_get_diets_amounts(self):
+        # Initialize
+        larp = create_all_2_groups()
+        # Obtain results
+        assigments = larp.get_character_assigments()
+        number_of_runs = get_runs_number(assigments)
+        players_diets = get_players_diets(assigments, number_of_runs)
+        diets_types = DietaryRestriction.objects.all()
+        all_runs_diets = get_diets_amounts(players_diets, number_of_runs, diets_types)
+        # Validate
+        self.assertIs(len(all_runs_diets), 2)
+        run = 0
+        for run_diets in all_runs_diets:
+            for diet in run_diets:
+                diet_name = diet["diet_name"]
+                example_amount = example_diets_counts[run][diet_name]
+                self.assertIs(diet["amount"], example_amount)
+            run += 1
 
     def test_get_food_empty(self):
         larp = Larp(name="")
