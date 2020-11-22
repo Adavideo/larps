9c3da8e95aaf27e291918609a09fd8bd16f40be4
Adavideo <adavideo@gmail.com>
2020-11-22 13:39:14 +0100

    Deleting secret key from production

--- notonlylarps/settings.py
+++ notonlylarps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i2vgc3fkk#283&5iw)spux@prkpd#v%scfw$!ie6tn3rl=ya5d'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
